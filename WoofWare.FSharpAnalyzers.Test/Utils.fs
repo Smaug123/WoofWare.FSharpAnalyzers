8629f7bc0a835a63cf664676150b6c31e1ca5001
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-11-23 09:31:37 +0000

    Format

--- WoofWare.FSharpAnalyzers.Test/Utils.fs
+++ WoofWare.FSharpAnalyzers.Test/Utils.fs
@@ -7,19 +7,23 @@ open NUnit.Framework
 [<TestFixture>]
 module Utils =
     [<Test ; Explicit "This isn't a test, but an easy way to obtain a TAST.">]
-    let getTast () = task {
-        let code = """module Foo
+    let getTast () =
+        task {
+            let code =
+                """module Foo
 let readPiped (stream: System.IO.Stream) =
       let buffer = Array.zeroCreate 1024
       stream.Read(buffer, 0, buffer.Length) |> ignore
       buffer
 """
-        let! options = ProjectOptions.get.Force ()
-        let ctx = code |> getContext options
-        let tree = Option.get ctx.TypedTree
-        for decl in tree.Declarations do
-            Console.Error.WriteLine decl
-            Console.Error.WriteLine "========"
-        ()
-    }
 
+            let! options = ProjectOptions.get.Force ()
+            let ctx = code |> getContext options
+            let tree = Option.get ctx.TypedTree
+
+            for decl in tree.Declarations do
+                Console.Error.WriteLine decl
+                Console.Error.WriteLine "========"
+
+            ()
+        }
