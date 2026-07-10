c893bcce0d047686b2a7b87451eedf8d351974e4
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2026-07-10 17:38:05 +0100

    Format

--- WoofWare.FSharpAnalyzers.Test/Data/TaskCompletionSourceAnalyzer/negative/Quotation.fs
+++ WoofWare.FSharpAnalyzers.Test/Data/TaskCompletionSourceAnalyzer/negative/Quotation.fs
@@ -4,5 +4,4 @@ open System.Threading.Tasks
 
 // A quotation only builds a code-as-data `Expr`; it does not construct a TaskCompletionSource, so
 // the constructor syntax inside it must not be flagged.
-let quoted () =
-    <@ TaskCompletionSource<int> () @>
+let quoted () = <@ TaskCompletionSource<int> () @>
